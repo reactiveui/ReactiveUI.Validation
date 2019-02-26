4a59cce3496ae378846cd7dcee26d9bee063de46
Glenn Watson <glenn@glennwatson.net>
2019-02-26 01:08:42 -0800

    housekeeping: Add android support/CI engine support
    Former-commit-id: 77fc15a38d9c9c247cc3a86e3bb34bc452487d47

---
+++ build.cake
@@ -0,0 +1,25 @@
+#load nuget:https://www.myget.org/F/reactiveui/api/v2?package=ReactiveUI.Cake.Recipe&prerelease
+
+Environment.SetVariableNames();
+
+// Whitelisted Packages
+var packageWhitelist = new[] 
+{ 
+    MakeAbsolute(File("./src/ReactiveUI.Validation/ReactiveUI.Validation.csproj")),
+};
+
+var packageTestWhitelist = new FilePath[]
+{
+};
+
+BuildParameters.SetParameters(context: Context, 
+                            buildSystem: BuildSystem,
+                            title: "ReactiveUI.Validation",
+                            whitelistPackages: packageWhitelist,
+                            whitelistTestPackages: packageTestWhitelist,
+                            artifactsDirectory: "./artifacts",
+                            sourceDirectory: "./src");
+
+ToolSettings.SetToolSettings(context: Context);
+
+Build.Run();
---
+++ src/ReactiveUI.Validation/Platforms/Android/ViewForExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq.Expressions;
+using Android.Support.Design.Widget;
+using ReactiveUI.Validation.Abstractions;
+using ReactiveUI.Validation.Formatters;
+using ReactiveUI.Validation.Helpers;
+using ReactiveUI.Validation.ValidationBindings;
+
+namespace ReactiveUI.Validation.DroidExtensions.Extensions
+{
+    public static class ViewForExtensions
+    {
+        /// <summary>
+        /// Platform binding to the <see cref="TextInputLayout"/>
+        /// </summary>
+        /// <typeparam name="TView"></typeparam>
+        /// <typeparam name="TViewModel"></typeparam>
+        /// <typeparam name="TViewModelProp"></typeparam>
+        /// <param name="view"></param>
+        /// <param name="viewModel"></param>
+        /// <param name="viewModelProperty"></param>
+        /// <param name="viewProperty"></param>
+        /// <returns></returns>
+        public static IDisposable BindValidation
+            <TView, TViewModel, TViewModelProp>(this TView view,
+                                                TViewModel viewModel,
+                                                Expression<Func<TViewModel, TViewModelProp>> viewModelProperty,
+                                                TextInputLayout viewProperty)
+            where TViewModel : ReactiveObject, ISupportsValidation
+            where TView : IViewFor<TViewModel>
+        {
+            return ValidationBinding.ForProperty(view, viewModelProperty,
+                (state, errorText) => viewProperty.Error = errorText, SingleLineFormatter.Default);
+        }
+
+        /// <summary>
+        /// Platform binding to the <see cref="TextInputLayout"/>
+        /// </summary>
+        /// <typeparam name="TView"></typeparam>
+        /// <typeparam name="TViewModel"></typeparam>
+        /// <param name="view"></param>
+        /// <param name="viewModel"></param>
+        /// <param name="viewModelHelperProperty"></param>
+        /// <param name="viewProperty"></param>
+        /// <returns></returns>
+        public static IDisposable BindValidation
+            <TView, TViewModel>(this TView view,
+                                TViewModel viewModel,
+                                Expression<Func<TViewModel, ValidationHelper>> viewModelHelperProperty,
+                                TextInputLayout viewProperty)
+            where TViewModel : ReactiveObject, ISupportsValidation
+            where TView : IViewFor<TViewModel>
+        {
+            return ValidationBinding.ForValidationHelperProperty(view, viewModelHelperProperty,
+                (state, errorText) =>
+                {
+                    viewProperty.Error = errorText;
+                }, SingleLineFormatter.Default);
+        }
+    }
+}
