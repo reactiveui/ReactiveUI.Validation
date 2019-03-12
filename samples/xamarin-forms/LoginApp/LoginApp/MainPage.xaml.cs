ae9d7bfb8b83e0f9df28b6678e382a0275450249
alexmartinezm <alexmdevs@gmail.com>
2019-03-12 12:14:10 +0100

    Added blank sample poroject for XF.

---
+++ samples/xamarin-forms/LoginApp/LoginApp/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoginApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
