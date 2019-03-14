04a9a3477e0b51b20d5101286756324bbf56e68d
alexmartinezm <alexmdevs@gmail.com>
2019-03-14 17:25:42 +0100

    [fix-multiple-validations-3] Added comments in ViewFor mixins.

--- src/ReactiveUI.Validation/Extensions/ViewForExtensions.cs
+++ src/ReactiveUI.Validation/Extensions/ViewForExtensions.cs
@@ -4,13 +4,17 @@ using System.Reactive.Linq;
 using ReactiveUI.Validation.Abstractions;
 using ReactiveUI.Validation.Helpers;
 using ReactiveUI.Validation.ValidationBindings;
+using Splat;
 
 namespace ReactiveUI.Validation.Extensions
 {
+    /// <summary>
+    /// Extensions methods associated to <see cref="IViewFor"/> instances.
+    /// </summary>
     public static class ViewForExtensions
     {
         /// <summary>
-        ///     Bind the specified view property validation to the view property.
+        /// Bind the specified ViewModel property validation to the View property.
         /// </summary>
         /// <typeparam name="TView"></typeparam>
         /// <typeparam name="TViewModel"></typeparam>
@@ -18,23 +22,21 @@ namespace ReactiveUI.Validation.Extensions
         /// <typeparam name="TViewProperty"></typeparam>
         /// <param name="view"></param>
         /// <param name="viewModel"></param>
-        /// <param name="viewModelProperty1"></param>
+        /// <param name="viewModelProperty"></param>
         /// <param name="viewProperty"></param>
         /// <returns></returns>
-        public static IDisposable BindValidation
-            <TView, TViewModel, TViewModelProperty1, TViewProperty>(this TView view,
-                TViewModel viewModel,
-                Expression<Func<TViewModel, TViewModelProperty1>>
-                    viewModelProperty1,
-                Expression<Func<TView, TViewProperty>> viewProperty)
+        public static IDisposable BindValidation<TView, TViewModel, TViewModelProperty1, TViewProperty>(this TView view,
+            TViewModel viewModel,
+            Expression<Func<TViewModel, TViewModelProperty1>> viewModelProperty,
+            Expression<Func<TView, TViewProperty>> viewProperty)
             where TViewModel : ReactiveObject, ISupportsValidation
             where TView : IViewFor<TViewModel>
         {
-            return ValidationBinding.ForProperty(view, viewModelProperty1, viewProperty);
+            return ValidationBinding.ForProperty(view, viewModelProperty, viewProperty);
         }
 
         /// <summary>
-        ///     Bind the overall validation of a view model to a specified view property.
+        /// Bind the overall validation of a View model to a specified View property.
         /// </summary>
         /// <typeparam name="TView"></typeparam>
         /// <typeparam name="TViewModel"></typeparam>
@@ -43,19 +45,17 @@ namespace ReactiveUI.Validation.Extensions
         /// <param name="viewModel"></param>
         /// <param name="viewProperty"></param>
         /// <returns></returns>
-        public static IDisposable BindValidation
-            <TView, TViewModel, TViewProperty>(this TView view,
-                TViewModel viewModel,
-                Expression<Func<TView, TViewProperty>> viewProperty)
+        public static IDisposable BindValidation<TView, TViewModel, TViewProperty>(this TView view,
+            TViewModel viewModel,
+            Expression<Func<TView, TViewProperty>> viewProperty)
             where TViewModel : ReactiveObject, ISupportsValidation
             where TView : IViewFor<TViewModel>
         {
             return ValidationBinding.ForViewModel<TView, TViewModel, TViewProperty>(view, viewProperty);
         }
 
-
         /// <summary>
-        ///     Bind a <see cref="ValidationHelper" /> from a view model to a specified view property.
+        /// Bind a <see cref="ValidationHelper" /> from a ViewModel to a specified View property.
         /// </summary>
         /// <typeparam name="TView"></typeparam>
         /// <typeparam name="TViewModel"></typeparam>
@@ -65,29 +65,25 @@ namespace ReactiveUI.Validation.Extensions
         /// <param name="viewModelHelperProperty"></param>
         /// <param name="viewProperty"></param>
         /// <returns></returns>
-        public static IDisposable BindValidation
-            <TView, TViewModel, TViewProperty>(this TView view,
-                TViewModel viewModel,
-                Expression<Func<TViewModel, ValidationHelper>> viewModelHelperProperty,
-                Expression<Func<TView, TViewProperty>> viewProperty)
+        public static IDisposable BindValidation<TView, TViewModel, TViewProperty>(this TView view,
+            TViewModel viewModel,
+            Expression<Func<TViewModel, ValidationHelper>> viewModelHelperProperty,
+            Expression<Func<TView, TViewProperty>> viewProperty)
             where TViewModel : ReactiveObject, ISupportsValidation
             where TView : IViewFor<TViewModel>
         {
             return ValidationBinding.ForValidationHelperProperty(view, viewModelHelperProperty, viewProperty);
         }
 
-
-        public static IDisposable BindToDirect
-            <TTarget, TValue>(IObservable<TValue> This, TTarget target, Expression viewExpression)
+        public static IDisposable BindToDirect<TTarget, TValue>(IObservable<TValue> This,
+            TTarget target,
+            Expression viewExpression)
         {
             var setter = Reflection.GetValueSetterOrThrow(viewExpression.GetMemberInfo());
             if (viewExpression.GetParent().NodeType == ExpressionType.Parameter)
                 return This.Subscribe(
                     x => setter(target, x, viewExpression.GetArgumentsArray()),
-                    ex =>
-                    {
-                        //this.Log().ErrorException(String.Format("{0} Binding received an Exception!", viewExpression), ex);
-                    });
+                    ex => LogHost.Default.ErrorException($"{viewExpression} Binding received an Exception!", ex));
 
             var bindInfo = This.CombineLatest(target.WhenAnyDynamic(viewExpression.GetParent(), x => x.Value),
                 (val, host) => new {val, host});
@@ -96,10 +92,7 @@ namespace ReactiveUI.Validation.Extensions
                 .Where(x => x.host != null)
                 .Subscribe(
                     x => setter(x.host, x.val, viewExpression.GetArgumentsArray()),
-                    ex =>
-                    {
-                        //this.Log().ErrorException(String.Format("{0} Binding received an Exception!", viewExpression), ex);
-                    });
+                    ex => LogHost.Default.ErrorException($"{viewExpression} Binding received an Exception!", ex));
         }
     }
 }
