679d0f0bff19ce452a95d1b9ee9b84a3c4d728a3
alexmartinezm <alexmdevs@gmail.com>
2019-03-14 20:54:20 +0100

    Removed unnecessary usings.

--- samples/xamarin-forms/LoginApp/LoginApp/ViewModels/SignUpViewModel.cs
+++ samples/xamarin-forms/LoginApp/LoginApp/ViewModels/SignUpViewModel.cs
@@ -3,9 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Linq;
 using System.Reactive;
-using System.Reactive.Disposables;
 using Acr.UserDialogs;
 using LoginApp.ViewModels.Abstractions;
 using ReactiveUI;
