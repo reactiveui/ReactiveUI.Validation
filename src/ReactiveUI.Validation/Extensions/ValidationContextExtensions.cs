8bbc3692f37fe91770c4edd72b35829be6966ad2
alexmartinezm <alexmdevs@gmail.com>
2019-03-14 17:36:09 +0100

    [fix-multiple-validations-3] Added comments in extension methods.

--- src/ReactiveUI.Validation/Extensions/ValidationContextExtensions.cs
+++ src/ReactiveUI.Validation/Extensions/ValidationContextExtensions.cs
@@ -7,6 +7,9 @@ using ReactiveUI.Validation.TemplateGenerators;
 
 namespace ReactiveUI.Validation.Extensions
 {
+    /// <summary>
+    /// Extensions methods for <see cref="ValidationContext"/>.
+    /// </summary>
     public static class ValidationContextExtensions
     {
         /// <summary>
@@ -20,13 +23,25 @@ namespace ReactiveUI.Validation.Extensions
             Expression<Func<TViewModel, TProperty1>> viewModelProperty1,
             bool strict = true)
         {
-            var instance = context.Validations.Where(p => p is BasePropertyValidation<TViewModel, TProperty1>)
+            var instance = context.Validations
+                .Where(p => p is BasePropertyValidation<TViewModel, TProperty1>)
                 .Cast<BasePropertyValidation<TViewModel, TProperty1>>()
                 .FirstOrDefault(v => v.ContainsProperty(viewModelProperty1, strict));
 
             return instance;
         }
 
+        /// <summary>
+        /// Resolve the property valuation for two properties.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="viewModelProperty1"></param>
+        /// <param name="viewModelProperty2"></param>
+        /// <param name="strict"></param>
+        /// <typeparam name="TViewModel"></typeparam>
+        /// <typeparam name="TProperty1"></typeparam>
+        /// <typeparam name="TProperty2"></typeparam>
+        /// <returns></returns>
         public static BasePropertyValidation<TViewModel, TProperty1, TProperty2> ResolveFor<TViewModel, TProperty1,
             TProperty2>(this ValidationContext context,
             Expression<Func<TViewModel, TProperty1>> viewModelProperty1,
@@ -34,14 +49,29 @@ namespace ReactiveUI.Validation.Extensions
             bool strict = true)
         {
             var instance = context
-                .Validations.Where(p => p is BasePropertyValidation<TViewModel, TProperty1, TProperty2>)
-                .Cast<BasePropertyValidation<TViewModel, TProperty1, TProperty2>>().FirstOrDefault(v =>
+                .Validations
+                .Where(p => p is BasePropertyValidation<TViewModel, TProperty1, TProperty2>)
+                .Cast<BasePropertyValidation<TViewModel, TProperty1, TProperty2>>()
+                .FirstOrDefault(v =>
                     v.ContainsProperty(viewModelProperty1) && v.ContainsProperty(viewModelProperty2) &&
                     v.PropertyCount == 2);
 
             return instance;
         }
 
+        /// <summary>
+        /// Resolve the property valuation for three properties.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="viewModelProperty1"></param>
+        /// <param name="viewModelProperty2"></param>
+        /// <param name="viewModelProperty3"></param>
+        /// <param name="strict"></param>
+        /// <typeparam name="TViewModel"></typeparam>
+        /// <typeparam name="TProperty1"></typeparam>
+        /// <typeparam name="TProperty2"></typeparam>
+        /// <typeparam name="TProperty3"></typeparam>
+        /// <returns></returns>
         public static BasePropertyValidation<TViewModel, TProperty1, TProperty2, TProperty3> ResolveFor<TViewModel,
             TProperty1, TProperty2, TProperty3>(this ValidationContext context,
             Expression<Func<TViewModel, TProperty1>> viewModelProperty1,
