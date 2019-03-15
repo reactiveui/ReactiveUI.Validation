2b3a9c90da402ac73f4dc4db782b3ffc370c8515
alexmartinezm <alexmdevs@gmail.com>
2019-03-15 12:18:56 +0100

    [fix-multiple-validations-3] Added action mixing for Android Extensions (the only one which uses it).

--- src/ReactiveUI.Validation/Platforms/Android/ViewForExtensions.cs
+++ src/ReactiveUI.Validation/Platforms/Android/ViewForExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Linq.Expressions;
 using Android.Support.Design.Widget;
 using ReactiveUI.Validation.Abstractions;
@@ -8,6 +9,9 @@ using ReactiveUI.Validation.ValidationBindings;
 
 namespace ReactiveUI.Validation.DroidExtensions.Extensions
 {
+    /// <summary>
+    /// Android specific extensions methods associated to <see cref="IViewFor"/> instances.
+    /// </summary>
     public static class ViewForExtensions
     {
         /// <summary>
@@ -21,11 +25,10 @@ namespace ReactiveUI.Validation.DroidExtensions.Extensions
         /// <param name="viewModelProperty"></param>
         /// <param name="viewProperty"></param>
         /// <returns></returns>
-        public static IDisposable BindValidation
-            <TView, TViewModel, TViewModelProp>(this TView view,
-                                                TViewModel viewModel,
-                                                Expression<Func<TViewModel, TViewModelProp>> viewModelProperty,
-                                                TextInputLayout viewProperty)
+        public static IDisposable BindValidation<TView, TViewModel, TViewModelProp>(this TView view,
+            TViewModel viewModel,
+            Expression<Func<TViewModel, TViewModelProp>> viewModelProperty,
+            TextInputLayout viewProperty)
             where TViewModel : ReactiveObject, ISupportsValidation
             where TView : IViewFor<TViewModel>
         {
@@ -38,24 +41,44 @@ namespace ReactiveUI.Validation.DroidExtensions.Extensions
         /// </summary>
         /// <typeparam name="TView"></typeparam>
         /// <typeparam name="TViewModel"></typeparam>
+        /// <typeparam name="TViewModelProp"></typeparam>
+        /// <param name="view"></param>
+        /// <param name="viewModel"></param>
+        /// <param name="viewModelProperty"></param>
+        /// <param name="viewProperty"></param>
+        /// <returns></returns>
+        public static IDisposable BindValidationEx<TView, TViewModel, TViewModelProp>(this TView view,
+            TViewModel viewModel,
+            Expression<Func<TViewModel, TViewModelProp>> viewModelProperty,
+            TextInputLayout viewProperty)
+            where TViewModel : ReactiveObject, ISupportsValidation
+            where TView : IViewFor<TViewModel>
+        {
+            return ValidationBindingEx.ForProperty(view,
+                viewModelProperty,
+                (state, errors) => viewProperty.Error = errors.FirstOrDefault(msg => !string.IsNullOrEmpty(msg)),
+                SingleLineFormatter.Default);
+        }
+
+        /// <summary>
+        /// Platform binding to the <see cref="TextInputLayout"/>
+        /// </summary>
+        /// <typeparam name="TView"></typeparam>
+        /// <typeparam name="TViewModel"></typeparam>
         /// <param name="view"></param>
         /// <param name="viewModel"></param>
         /// <param name="viewModelHelperProperty"></param>
         /// <param name="viewProperty"></param>
         /// <returns></returns>
-        public static IDisposable BindValidation
-            <TView, TViewModel>(this TView view,
-                                TViewModel viewModel,
-                                Expression<Func<TViewModel, ValidationHelper>> viewModelHelperProperty,
-                                TextInputLayout viewProperty)
+        public static IDisposable BindValidation<TView, TViewModel>(this TView view,
+            TViewModel viewModel,
+            Expression<Func<TViewModel, ValidationHelper>> viewModelHelperProperty,
+            TextInputLayout viewProperty)
             where TViewModel : ReactiveObject, ISupportsValidation
             where TView : IViewFor<TViewModel>
         {
             return ValidationBinding.ForValidationHelperProperty(view, viewModelHelperProperty,
-                (state, errorText) =>
-                {
-                    viewProperty.Error = errorText;
-                }, SingleLineFormatter.Default);
+                (state, errorText) => { viewProperty.Error = errorText; }, SingleLineFormatter.Default);
         }
     }
 }
