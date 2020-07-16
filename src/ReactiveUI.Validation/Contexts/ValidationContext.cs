ccba39010e76f91841371130b9dbc5fbfe5447c1
artyom <worldbeater-dev@yandex.ru>
2020-07-16 23:21:34 +0300

    Bring back the scheduling fix until we ensure it finally works without it

--- src/ReactiveUI.Validation/Contexts/ValidationContext.cs
+++ src/ReactiveUI.Validation/Contexts/ValidationContext.cs
@@ -53,7 +53,11 @@ namespace ReactiveUI.Validation.Contexts
         /// <param name="scheduler">Optional scheduler to use for the properties. Uses the main thread scheduler by default.</param>
         public ValidationContext(IScheduler scheduler = null)
         {
-            _scheduler = scheduler ?? RxApp.MainThreadScheduler;
+#if NET_461 || NETSTANDARD
+            scheduler = scheduler ?? RxApp.TaskpoolScheduler;
+#else
+            scheduler = scheduler ?? RxApp.MainThreadScheduler;
+#endif
             var validationChangedObservable = _validationSource.Connect();
 
             _validations = validationChangedObservable
