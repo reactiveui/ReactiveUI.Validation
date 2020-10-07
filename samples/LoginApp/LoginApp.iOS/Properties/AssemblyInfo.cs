5babb959a277aec13a140db4671e79bbe905a89d
Artyom <worldbeater-dev@yandex.ru>
2020-10-07 12:26:41 +0300

    Use the .NET Foundation header in samples

--- samples/LoginApp/LoginApp.iOS/Properties/AssemblyInfo.cs
+++ samples/LoginApp/LoginApp.iOS/Properties/AssemblyInfo.cs
@@ -1,8 +1,7 @@
-﻿// <copyright file="ReactiveUI.Validation/samples/xamarin-forms/LoginApp/LoginApp.iOS/Properties/AssemblyInfo.cs" company=".NET Foundation">
+﻿// Copyright (c) 2020 .NET Foundation and Contributors. All rights reserved.
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-// </copyright>
+// See the LICENSE file in the project root for full license information.
 
 using System.Reflection;
 using System.Runtime.InteropServices;
