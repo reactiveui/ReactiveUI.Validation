aec03847db92e95282c8a64954142dd815965e15
Artyom V. Gorchakov <worldbeater-dev@yandex.ru>
2020-10-10 02:22:50 +0300

    Add LoginApp.WinForms and LoginApp.Uwp (#102)
    * Basic sample app for Windows Forms
    
    * Add a UWP sample app
    
    * Add separate sln files for each platform
    
    * Move LoginApp projects to samples/
    
    * Fix project path

---
+++ samples/LoginApp.Uwp/Services/UwpUserDialogs.cs
@@ -0,0 +1,19 @@
+// Copyright (c) 2020 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using Windows.UI.Popups;
+using LoginApp.Services;
+
+namespace LoginApp.Uwp.Services
+{
+    /// <summary>
+    /// This class defines user dialogs for the Universal Windows Platform app.
+    /// </summary>
+    public class UwpUserDialogs : IUserDialogs
+    {
+        /// <inheritdoc />
+        public void ShowDialog(string message) => new MessageDialog(message).ShowAsync();
+    }
+}
---
+++ samples/LoginApp.WinForms/Program.cs
@@ -0,0 +1,29 @@
+// Copyright (c) 2020 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Windows.Forms;
+using LoginApp.WinForms.Views;
+
+namespace LoginApp.WinForms
+{
+    /// <summary>
+    /// Windows Forms entry point.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SignUpView());
+        }
+    }
+}
