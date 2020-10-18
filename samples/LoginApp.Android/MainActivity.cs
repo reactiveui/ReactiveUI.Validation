c2a0291f7a05ff021bf4b2a4281c3be4330a3a87
Artjom Graf <prizrak.nra@gmail.com>
2020-10-18 23:55:09 +0300

    Basic inputs

--- samples/LoginApp.Android/MainActivity.cs
+++ samples/LoginApp.Android/MainActivity.cs
@@ -1,16 +1,15 @@
 ﻿using Android.App;
 using Android.OS;
-using Android.Support.V7.App;
+using LoginApp.ViewModels;
 
 namespace LoginApp.Droid
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    [Activity(Label = "LoginApp", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : ReactiveUI.ReactiveActivity<SignUpViewModel>
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
         }
     }
