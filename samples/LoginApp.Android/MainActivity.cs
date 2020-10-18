c4a8affd14c627bb935f730335105cd22c03e83d
Artjom Graf <prizrak.nra@gmail.com>
2020-10-18 23:34:16 +0300

    Add a basic native Android app

---
+++ samples/LoginApp.Android/MainActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace LoginApp.Droid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
