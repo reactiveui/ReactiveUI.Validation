44100c567ba46a40fefa78bba0efcb7156350ed4
Artjom Graf <prizrak.nra@gmail.com>
2020-10-19 16:55:43 +0300

    Fix UWP warnings

--- samples/LoginApp.Uwp/Services/UwpUserDialogs.cs
+++ samples/LoginApp.Uwp/Services/UwpUserDialogs.cs
@@ -3,8 +3,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
-using Windows.UI.Popups;
 using LoginApp.Services;
+using Windows.UI.Popups;
 
 namespace LoginApp.Uwp.Services
 {
