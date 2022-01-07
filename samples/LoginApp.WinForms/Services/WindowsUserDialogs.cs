daccf0e5f75cc4c809be91c5b2975b7b6e4a8db3
Artjom Graf <prizrak.nra@gmail.com>
2022-01-07 19:20:12 +0300

    Fix Windows Forms build

--- samples/LoginApp.WinForms/Services/WindowsUserDialogs.cs
+++ samples/LoginApp.WinForms/Services/WindowsUserDialogs.cs
@@ -3,7 +3,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
-using System.Windows;
+using System.Windows.Forms;
 using LoginApp.Services;
 
 namespace LoginApp.WinForms.Services
