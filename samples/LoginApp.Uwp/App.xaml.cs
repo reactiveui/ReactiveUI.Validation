9146f19e26b7b0595bf0355dd23e1c7d9f7c3663
Artyom V. Gorchakov <worldbeater-dev@yandex.ru>
2022-01-08 14:42:03 +0300

    housekeeping: Update copyright year, apply more refactorings (#415)
    * Update copyright year
    * More file scoped namespaces

--- samples/LoginApp.Uwp/App.xaml.cs
+++ samples/LoginApp.Uwp/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2021 .NET Foundation and Contributors. All rights reserved.
+﻿// Copyright (c) 2022 .NET Foundation and Contributors. All rights reserved.
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
@@ -8,40 +8,39 @@ using Windows.ApplicationModel.Activation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
-namespace LoginApp.Uwp
+namespace LoginApp.Uwp;
+
+/// <summary>
+/// Defines the main Universal Windows Application class.
+/// </summary>
+/// <inheritdoc />
+public sealed partial class App : Application
 {
     /// <summary>
-    /// Defines the main Universal Windows Application class.
+    /// Initializes a new instance of the <see cref="App"/> class.
     /// </summary>
+    public App() => InitializeComponent();
+
     /// <inheritdoc />
-    public sealed partial class App : Application
+    protected override void OnLaunched(LaunchActivatedEventArgs e)
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="App"/> class.
-        /// </summary>
-        public App() => InitializeComponent();
-
-        /// <inheritdoc />
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        Frame rootFrame = Window.Current.Content as Frame;
+        if (rootFrame == null)
         {
-            Frame rootFrame = Window.Current.Content as Frame;
-            if (rootFrame == null)
-            {
-                rootFrame = new Frame();
-                Window.Current.Content = rootFrame;
-            }
-
-            if (e?.PrelaunchActivated == true)
-            {
-                return;
-            }
-
-            if (rootFrame.Content == null)
-            {
-                rootFrame.Navigate(typeof(SignUpView), e?.Arguments);
-            }
-
-            Window.Current.Activate();
+            rootFrame = new Frame();
+            Window.Current.Content = rootFrame;
         }
+
+        if (e?.PrelaunchActivated == true)
+        {
+            return;
+        }
+
+        if (rootFrame.Content == null)
+        {
+            rootFrame.Navigate(typeof(SignUpView), e?.Arguments);
+        }
+
+        Window.Current.Activate();
     }
 }
--- samples/LoginApp.Uwp/Services/UwpUserDialogs.cs
+++ samples/LoginApp.Uwp/Services/UwpUserDialogs.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 .NET Foundation and Contributors. All rights reserved.
+// Copyright (c) 2022 .NET Foundation and Contributors. All rights reserved.
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
@@ -6,14 +6,13 @@
 using LoginApp.Services;
 using Windows.UI.Popups;
 
-namespace LoginApp.Uwp.Services
+namespace LoginApp.Uwp.Services;
+
+/// <summary>
+/// This class defines user dialogs for the Universal Windows Platform app.
+/// </summary>
+public class UwpUserDialogs : IUserDialogs
 {
-    /// <summary>
-    /// This class defines user dialogs for the Universal Windows Platform app.
-    /// </summary>
-    public class UwpUserDialogs : IUserDialogs
-    {
-        /// <inheritdoc />
-        public void ShowDialog(string message) => new MessageDialog(message).ShowAsync();
-    }
+    /// <inheritdoc />
+    public void ShowDialog(string message) => new MessageDialog(message).ShowAsync();
 }
